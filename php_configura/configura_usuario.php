0cf54e04c1fd6869ae9a26d8706e978802e0c405
odnols <gusbra21@outlook.com>
2024-08-01 20:57:47 -0300

    Atualizando jQuery

--- php_configura/configura_usuario.php
+++ php_configura/configura_usuario.php
@@ -167,12 +167,10 @@
 	<?php } ?>
 	</form>
 
-	<script type="text/javascript" src="../js/jquery.1.11.1.js"></script>
+	<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="../js/bootstrap.js"></script>
 	<script type="text/javascript" src="../js/SmoothScroll.js"></script>
 	<script type="text/javascript" src="../js/nivo-lightbox.js"></script>
-	<script type="text/javascript" src="../js/jquery.isotope.js"></script>
-	<script type="text/javascript" src="../js/jqBootstrapValidation.js"></script>
 	<script type="text/javascript" src="../js/main.js"></script>
 	<script type="text/javascript" src="../js/funcoes.js"></script>
 </body>
